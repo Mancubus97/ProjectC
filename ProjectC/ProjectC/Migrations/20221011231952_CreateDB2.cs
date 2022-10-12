323a315ab8f68e6362facf1ef17ee34ef2f88d75
Silvijn de Maat <1006512@hr.nl>
2022-10-12 19:27:59 +0200

    template code

---
+++ ProjectC/ProjectC/Migrations/20221011231952_CreateDB2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectC.Migrations
+{
+    public partial class CreateDB2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "EF");
+
+            migrationBuilder.CreateTable(
+                name: "innovations",
+                schema: "EF",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_innovations", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "innovations",
+                schema: "EF");
+        }
+    }
+}
---
+++ ProjectC/ProjectC/Models/ErrorViewModel.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Reflection.Emit;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProjectC.Models
+{
+    public record Innovation(Guid id, string name) { } //A Guid is like an int. Was recommended by a teacher to use for ID's.
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Innovation> innovations { get; set; }
+
+        public MyDbContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(@"Host=localhost:5432;Username=postgres;Password=;Database=projectc-db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema("EF");
+            modelBuilder.Entity<Innovation>().HasKey(_ => _.id);
+        }
+
+    }
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+
+
+    
+}
