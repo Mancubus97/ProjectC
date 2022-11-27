7e4d00a286b31e668e23a038c8af2761f72fd347
Silvijn de Maat <1006512@hr.nl>
2022-11-27 15:35:02 +0100

    444

--- ProjectC/ProjectC/Models/AccountModel.cs
+++ ProjectC/ProjectC/Models/AccountModel.cs
@@ -4,8 +4,8 @@
     {
 
         public Guid id { get; set; }
-        public string? username { get; set; }
-        public string? password { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
         public Account(Guid id, string username, string password)
         {
             this.id = id;
