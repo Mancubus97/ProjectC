ceb0143571feb07ba365d65dd09833682bb6c008
Silvijn de Maat <1006512@hr.nl>
2022-11-28 10:10:47 +0100

    777

--- ProjectC/ProjectC/Models/ErrorViewModel.cs
+++ ProjectC/ProjectC/Models/ErrorViewModel.cs
@@ -22,7 +22,7 @@ namespace ProjectC.Models
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
 
-            optionsBuilder.UseNpgsql(@"4.tcp.eu.ngrok.io:10074;Username=postgres;Password=;Database=projectc-db");
+            optionsBuilder.UseNpgsql(@"Host=4.tcp.eu.ngrok.io:10074;Username=postgres;Password=;Database=projectc-db");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
